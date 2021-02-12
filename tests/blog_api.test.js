34d5bde31936ae692f89e624ac9bd1e6514f2847
incredible92 <sulymanabdullahi@gmail.com>
2021-02-12 15:58:14 +0100

    Initializing database

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,11 +4,45 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Note(initialBlogs[1])
+  await blogObject.save()
+})
+
+
 test('blog are returned as json', async () => {
   await api
     .get('/api/blog')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body[0].content).toBe('HTML is easy')
 })
 
 afterAll(() => {
