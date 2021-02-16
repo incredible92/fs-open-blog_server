d460ea2ae4f2157a754ac74b2913c6109070bf55
incredible92 <sulymanabdullahi@gmail.com>
2021-02-16 17:14:16 +0100

    part4-ex-4.8-step1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,106 +1,51 @@
 const mongoose = require('mongoose')
-const supertest = require('supertest')
-const helper = require('./test_helper')
-const app = require('../app')
-
+import supertest from "supertest"
+import app from "../app"
 const api = supertest(app)
-
 const Blog = require('../models/blog')
-const initialBlogs = [
+
+const blogs = [
   {
-    content: 'HTML is easy',
-    date: new Date(),
-    important: false,
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
   },
   {
-    content: 'Browser can execute only Javascript',
-    date: new Date(),
-    important: true,
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:
+      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
   },
-]
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+];
+
 beforeEach(async () => {
-  await Blog.deleteMany({})
-
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+  await Blog.deleteMany({});
+  await new Blog(blogs[0]).save();
+  await new Blog(blogs[1]).save();
+  await new Blog(blogs[2]).save();
 })
 
-
-test('blog are returned as json', async () => {
-  console.log('entered test')
-  await api
-    .get('/api/blog')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('there are two blogs', async () => {
-  const response = await api.get('/api/blog')
-
-  expect(response.body).toHaveLength(2)
-})
-
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blog')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blog')
-
-  const contents = response.body.map(r => r.content)
-  expect(contents).toContain(
-    'Browser can execute only Javascript'
-  )
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    content: 'async/await simplifies making async calls',
-    important: true,
-  }
-
-  await api
-    .post('/api/blog')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  
-    const contents = blogsAtEnd.map(n => n.content)
-  
-
-  const response = await api.get('/api/blog')
-
-  const contents = response.body.map(r => r.content)
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(contents).toContain(
-    'async/await simplifies making async calls'
-  )
-})
-
-test('blog without content is not added', async () => {
-  const newBlog = {
-    important: true
-  }
-
-  await api
-    .post('/api/blog')
-    .send(newBlog)
-    .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-  
+describe('GET /blogs', function () {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 })
 
 afterAll(() => {
