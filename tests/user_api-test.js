5b478c6ebb3d252e74b1ba71259191b2b8554886
incredible92 <sulymanabdullahi@gmail.com>
2021-02-25 17:04:24 +0100

    user_api-test

--- tests/user_api-test.js
+++ tests/user_api-test.js
@@ -3,9 +3,9 @@ const supertest = require('supertest');
 const helper = require('../utils/test_helper');
 const app = require('../app');
 const api = supertest(app);
-const User = require('../models/user');
+const user = require('../models/user');
 
-const initialUsersList = [
+const initialUsers = [
   {
     name: 'Abdullahi Sulyman',
     username: 'mrincredible',
@@ -17,3 +17,11 @@ const initialUsersList = [
     password: 'hellosam',
   },
 ];
+
+beforeEach(async() => {
+    await user.deleteMany({})
+    const usersArray = initialUsers.map((user) => new User(user));
+    const promiseArray = usersArray.map((user) => user.save());
+
+  await Promise.all(promiseArray);
+})
