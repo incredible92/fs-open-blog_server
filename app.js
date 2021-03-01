f06b39798a5968e126323d040abae7344a869eea
incredible92 <sulymanabdullahi@gmail.com>
2021-03-01 10:35:54 +0100

    add:4.15 and 4.16

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+
 const express = require('express');
-require('express-async-errors')
 const cors = require('cors');
+require('express-async-errors');
+const mongoose = require('mongoose');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const blogRouter = require('./controllers/blog');
-const userRouter = require('./controllers/user')
+const userRouter = require('./controllers/user');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 
 const app = express();
@@ -14,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(config.mongoUrl, {
+  .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -27,18 +29,17 @@ mongoose
     logger.error('error connecting to MongoDB:', error.message);
   });
 
-// app.use(middleware.requestLogger);
-// app.use(middleware.tokenExtractor);
-
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 app.use('/api/blogs', blogRouter);
-// app.use('/api/users', userRouter);
-// app.use('/api/login', loginRouter);
+app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
-// if (process.env.NODE_ENV === 'test') {
-//   const testingRouter = require('./controllers/testing');
-//   app.use('/api/testing', testingRouter);
-// }
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
